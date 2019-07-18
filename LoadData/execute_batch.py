4ff861d770592085ea3867a262a769c26680e3ba
vathuluri <rain2showers@gmail.com>
2019-07-18 08:57:41 -0400

    added data load using python batch executre

--- LoadData/execute_batch.py
+++ LoadData/execute_batch.py
@@ -32,3 +32,72 @@ def insert_execute_batch(connection, beers: Iterator[Dict[str, Any]]) -> None:
                 %(volume)s
             );
         """, all_beers)
+
+
+def insert_execute_batch_iterator(connection, beers: Iterator[Dict[str, Any]]) -> None:
+    with connection.cursor() as cursor:
+        create_staging_table(cursor)
+
+        iter_beers = ({
+            **beer,
+            'first_brewed': parse_first_brewed(beer['first_brewed']),
+            'volume': beer['volume']['value'],
+        } for beer in beers)
+
+        psycopg2.extras.execute_batch(cursor, """
+            INSERT INTO staging_beers VALUES (
+                %(id)s,
+                %(name)s,
+                %(tagline)s,
+                %(first_brewed)s,
+                %(description)s,
+                %(image_url)s,
+                %(abv)s,
+                %(ibu)s,
+                %(target_fg)s,
+                %(target_og)s,
+                %(ebc)s,
+                %(srm)s,
+                %(ph)s,
+                %(attenuation_level)s,
+                %(brewers_tips)s,
+                %(contributed_by)s,
+                %(volume)s
+            );
+        """, iter_beers)
+
+def insert_execute_batch_iterator(
+    connection,
+    beers: Iterator[Dict[str, Any]],
+    page_size: int = 100,
+) -> None:
+    with connection.cursor() as cursor:
+        create_staging_table(cursor)
+
+        iter_beers = ({
+            **beer,
+            'first_brewed': parse_first_brewed(beer['first_brewed']),
+            'volume': beer['volume']['value'],
+        } for beer in beers)
+
+        psycopg2.extras.execute_batch(cursor, """
+            INSERT INTO staging_beers VALUES (
+                %(id)s,
+                %(name)s,
+                %(tagline)s,
+                %(first_brewed)s,
+                %(description)s,
+                %(image_url)s,
+                %(abv)s,
+                %(ibu)s,
+                %(target_fg)s,
+                %(target_og)s,
+                %(ebc)s,
+                %(srm)s,
+                %(ph)s,
+                %(attenuation_level)s,
+                %(brewers_tips)s,
+                %(contributed_by)s,
+                %(volume)s
+            );
+        """, iter_beers, page_size=page_size)
